de7654f9b7b7e300d00bf023313776eac9a91654
ivansmf23 <ivan9mendoza109@gmail.com>
2025-02-03 17:09:45 -0600

    cambios

--- Proyecto.java
+++ Proyecto.java
@@ -1,1 +1,24 @@
-import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Proyecto extends JFrame {
+
+    public Proyecto() {
+        setTitle("Gestión de Datos");
+        setSize(800, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        add(panel);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Proyecto().setVisible(true);
+            }
+        });
+    }
+}
